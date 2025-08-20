55c007fa9e393c967cc547154a8386515dd650fc
fgabaldo21 <fran.gabaldo@gmail.com>
2025-08-20 13:00:48 +0200

    updated requirements and gitignore, added a sorter script that sorts the dataset images to designated class folders

---
+++ data/sorter.py
@@ -0,0 +1,34 @@
+import mat4py
+import pandas as pd
+import os
+import shutil
+
+data = mat4py.loadmat('data/cars196/car_devkit/devkit/cars_train_annos.mat')
+
+classes = data['annotations'].get('class')
+
+images = data['annotations'].get('fname')
+
+root_name = "data/images"
+
+images_path = "data/cars196/cars_train/cars_train"
+
+if not os.path.exists(root_name):
+    os.makedirs(root_name)
+
+for i in range(1,197):
+    path = os.path.join(root_name, str(i))
+    
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+for i in range(len(images)):
+    old_path = os.path.join(images_path, images[i])
+    new_path = os.path.join(root_name, str(classes[i]), images[i])
+
+    shutil.move(old_path, new_path)
+
+    if i % 500 == 0:
+        print(f"Moved {i} images")
+
+print(f"Moved {len(images)} images")
