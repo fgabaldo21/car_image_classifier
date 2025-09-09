529cbc0f4ae1fe3c2ce42ec8e926d277d28c1127
fgabaldo21 <fran.gabaldo@gmail.com>
2025-09-09 10:50:24 +0200

    changes to the model

--- model/model.py
+++ model/model.py
@@ -4,51 +4,59 @@ import torch
 class Cnn(nn.Module):
     def __init__(self):
         super().__init__()
-
-        self.conv_block1 = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=4, kernel_size=3, stride=1),
-            nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, stride=1),
-            nn.LeakyReLU(negative_slope=0.2),
-
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, stride=1),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1),
-            nn.LeakyReLU(negative_slope=0.2)
-        )
-
-        self.conv_block2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=2, stride=1),
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=2, stride=1),
-            nn.LeakyReLU(negative_slope=0.2)
-        )
-
-        self.batchNorm = nn.BatchNorm2d(num_features=32, momentum=0.1)
-        self.drop = nn.Dropout2d(p=0.25)
-
-        self.sigmoid = nn.Sigmoid()
-
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=4, stride=1, padding=1)
+        
+        self.linear1 = nn.Linear(in_features=215296, out_features=512)
+        self.linear2 = nn.Linear(in_features=512, out_features=128)
+        self.linear3 = nn.Linear(in_features=128, out_features=10)
+        
+        self.dropout = nn.Dropout()
         self.flatten = nn.Flatten(start_dim=1)
-
-        self.linear = nn.Linear(in_features=15366400, out_features=196)
-
+        
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+        
     def forward(self, x):
-        x = self.conv_block1(x)
-
-        x = self.batchNorm(x)
-        x = self.drop(x)
-
-        x = self.conv_block2(x)
-
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.conv3(x)
+        x = self.relu(x)
+        
+        x = self.conv4(x)
+        x = self.relu(x)
+        
+        x = self.conv5(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.dropout(x)
+        
         x = self.flatten(x)
-
-        x = self.linear(x)
-        x = self.sigmoid(x)
-
+        
+        x = self.linear1(x)
+        x = self.relu(x)
+        
+        x = self.linear2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        
+        x = self.linear3(x)
+        
         return x
 
 # test block
 
-dummy_input = torch.randn(1, 1, 256, 256)
+dummy_input = torch.randn(1, 3, 256, 256)
 
 model = Cnn()
 
